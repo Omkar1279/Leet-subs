621647a8b04199beff9434b7b2d46ca57f45bd84
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:54:57 +0530

    Update LeetCode submission: Rotate Array

--- leetcode_submissions/2023-07-19/Rotate_Array_cpp.cpp
+++ leetcode_submissions/2023-07-19/Rotate_Array_cpp.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
 
-        k=k%nums.size();
-        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+        int n= nums.size();
 
-        reverse(nums.begin()+(nums.size()-k),nums.end());
-    
-        reverse(nums.begin(),nums.end());
+        for(int c=0;c<k;c++) {
+            int temp = nums[n-1];
+            for(int i=n-1;i>0;i--) {
+                nums[i]=nums[i-1];
+            }
+            nums[0]=temp;
+        }
     }
 };
