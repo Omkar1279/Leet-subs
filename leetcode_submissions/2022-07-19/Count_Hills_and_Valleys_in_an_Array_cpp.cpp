238ee0bf7e166d5c3b775fa0b90433775ba30575
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:58:53 +0530

    Update LeetCode submission: Count Hills and Valleys in an Array

--- leetcode_submissions/2022-07-19/Count_Hills_and_Valleys_in_an_Array_cpp.cpp
+++ leetcode_submissions/2022-07-19/Count_Hills_and_Valleys_in_an_Array_cpp.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        int n=nums.size();
-        int ans=0;
-        for(int i=1;i<n-1;i++){
-            if(nums[i]==nums[i-1]){
-                continue;
-            }
+        
+        int n=nums.size(),ans=0;
+        
+        for(int i=1;i<n-1;i++) {
             
-            else{
+            if(nums[i]==nums[i-1]) continue;
+            
+            else {
+            
                 int j=i+1;
-                while(j!=n && nums[i]==nums[j])
-                    j++;
-                
-                if( j!=n && nums[i-1]>nums[i] && nums[j]>nums[i] )
-                    ans++;
-                    
-                else if( j!=n &&  nums[i-1]<nums[i] && nums[j]<nums[i] )
-                    ans++;       
+
+                while(nums[i]==nums[j] && j!=n) j++;
+
+                if( j!=n && nums[i] < nums[j] && nums[i] < nums[i-1]) ans++;
+
+                else if( j!=n && nums[i] > nums[j] && nums[i] > nums[i-1]) ans++;
             }
         }
-        
         return ans;
     }
 };
