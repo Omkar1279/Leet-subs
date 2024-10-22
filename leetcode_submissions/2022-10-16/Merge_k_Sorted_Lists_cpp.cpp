b8442591459abdffd0c56d55e13fd8aaec7d9941
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:56:00 +0530

    Add LeetCode submission: Merge k Sorted Lists

---
+++ leetcode_submissions/2022-10-16/Merge_k_Sorted_Lists_cpp.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        if(!l1 && !l2) return NULL;
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        return new ListNode((l1->val <= l2->val) ? l1->val : l2->val,(l1->val <= l2->val) ? mergeTwoLists(l1->next, l2) : mergeTwoLists(l1, l2->next));        
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = mergeTwoLists(head, lists[i]);
+        
+        return head;
+    }
+};
