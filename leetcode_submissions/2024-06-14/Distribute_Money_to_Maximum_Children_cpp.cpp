9f13d3887267356cec2acbf887fca7f7d9caa815
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:53:52 +0530

    Add LeetCode submission: Distribute Money to Maximum Children

---
+++ leetcode_submissions/2024-06-14/Distribute_Money_to_Maximum_Children_cpp.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if(money<children) return -1;
+
+        money-=children;
+
+        if (money / 7 >= children) {
+            return children;
+        }
+
+        int max_eight_dollar_children = money / 7;
+
+        int remaining_money = money - max_eight_dollar_children * 7;
+
+        
+        if (remaining_money == 3 && (children - 
+        max_eight_dollar_children == 1)) {
+            max_eight_dollar_children--;
+        }
+
+        return max_eight_dollar_children;  
+    }
+};
