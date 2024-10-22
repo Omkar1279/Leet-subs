25d52c891a83313b3cabb432cac88a0df95fb26e
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:57:47 +0530

    Update LeetCode submission: Reverse Integer

--- leetcode_submissions/2022-08-19/Reverse_Integer_cpp.cpp
+++ leetcode_submissions/2022-08-19/Reverse_Integer_cpp.cpp
@@ -4,20 +4,14 @@ public:
     
     int reverse(int x) {
         
-        int l=0,r=0;
-        string s=to_string(x);
+        int ans=0,t=x;
+        if(x<0) t=(-1)*t;
         
-        r=s.size()-1;
-        
-        while(l<=r) {
-            // cout<<l<<" "<<r<<endl;
-            swap(s[l],s[r]);
-            l++;r--;
+        while(t>0) {
+            ans=ans*10+t%10;
+            t/=10;
         }
-        long ans=stol(s);
-        
-        if(ans<pow(-2,31)||ans>pow(2,31)-1) return 0;
-        
-        return (x<0) ?(int) 0-ans : (int)ans;
+        if(x<0) ans=(-1)*ans;
+        return ans;
     }
 };
