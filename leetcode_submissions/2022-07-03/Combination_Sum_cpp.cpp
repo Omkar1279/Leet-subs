5e100065f04e405ceacd72888dbb284a42abaf19
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:59:18 +0530

    Add LeetCode submission: Combination Sum

---
+++ leetcode_submissions/2022-07-03/Combination_Sum_cpp.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int i,set<vector<int>> &s,vector<int>& c,int t,vector<int> temp) {
+        
+        if(i==c.size()) {
+            sort(temp.begin(),temp.end());
+            if(accumulate(temp.begin(),temp.end(),0) == t) s.insert(temp);
+            return;
+        }
+        else if(accumulate(temp.begin(),temp.end(),0) > t) return;
+        
+        temp.push_back(c[i]);
+        solve(i,s,c,t,temp);
+        temp.pop_back();
+        solve(i+1,s,c,t,temp);
+            
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        set<vector<int>>s;vector<int> temp;
+        solve(0,s,candidates,target,temp);
+        
+        vector<vector<int>> v (s.begin(),s.end());
+        return v;
+    }
+};
