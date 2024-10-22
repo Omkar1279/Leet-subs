79b47fc2f4ef4d8c79c77da060635bb0c2799909
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:56:51 +0530

    Add LeetCode submission: Removing Stars From a String

---
+++ leetcode_submissions/2022-08-28/Removing_Stars_From_a_String_cpp.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        
+        string ans="";stack<char> st;
+//         for(int i=0;i<s.size();i++) {
+            
+//             if(s[i]=='*' && i>0) {s.erase(i-1,2);i-=2;}
+            
+//         }
+        
+        for(int i=0;i<s.size();i++) {
+            
+            if(s[i]=='*' && !st.empty()) st.pop();
+            else st.push(s[i]);
+        }
+        
+        while(!st.empty()) {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
