0763dd5e99e51d5b38819fede0b0f028c370901c
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 19:57:59 +0530

    Add LeetCode submission: Find Kth Bit in Nth Binary String

---
+++ leetcode_submissions/2024-07-21/1667. Find Kth Bit in Nth Binary String/1667. Find Kth Bit in Nth Binary String/Find_Kth_Bit_in_Nth_Binary_String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverse_invert(string &s) {
+        for(char &c:s) {
+            if(c=='1') c='0';
+            else c='1';
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+
+    string find(int n) {
+        if(n==1) {
+            return "0";
+        }
+        string t= find(n-1); 
+        return t + "1" + reverse_invert(t);
+    }
+    char findKthBit(int n, int k) {
+        string ans=find(n);
+        
+        return ans[k-1];
+    }
+};
