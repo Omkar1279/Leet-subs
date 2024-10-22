84a6f62101e87e6aea9f1446b817b6e2c144292c
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:58:09 +0530

    Update LeetCode submission: Number of Subsequences That Satisfy the Given Sum Condition

--- leetcode_submissions/2022-08-16/Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition_cpp.cpp
+++ leetcode_submissions/2022-08-16/Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition_cpp.cpp
@@ -1,32 +1,30 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    int power(long long x, long long y, long long p) {
-            long long res = 1;
-            x = x % p;
-            if (x == 0) return 0;
-            while (y > 0)  {
-                if (y & 1)
-                    res = (res*x) % p;
-                y = y>>1;
-                x = ((x%p)*(x%p)) % p;
-            }
-            return (int)(res%p);
-    }
-    
     int numSubseq(vector<int>& nums, int t) {
         
-        int n=nums.size(),cnt=0;
-        long MOD=1e9+7;
+        int n=nums.size(),cnt=0,MOD=1e9+7;
         
         sort(nums.begin(),nums.end());
         
-        int l=0,r=n-1;                
-        while(r>=l) {
+//         for(int i=0;i<n-1;i++) {
+//             if(2*nums[i]<=t) cnt++;
+        
+//             for(int j=n-1;j>i;j--) {
+                
+//                 if(nums[i]+nums[j]<=t) 
+//                     cnt+=pow(2,j-i-1);
+        
+//                 // cout<<"i="<<i<<" j="<<j<<" cnt="<<cnt<<endl;
+//             }
+//         }
+        
+        int l=0,r=n-1;        
+        
+        while(l<=r) {
             
             if(nums[l]+nums[r]<=t) {
-                cnt=((cnt%MOD)+(power(2, r - l, MOD)%MOD)%MOD);
-                l++;
+                cnt+=(int)(pow(2,r-l))%MOD;l++;
             }
             else r--;
         }
