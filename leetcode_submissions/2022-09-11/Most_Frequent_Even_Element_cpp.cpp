6f5ee9fede094b83fda139d6ddcc77fcb5554f2a
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:56:40 +0530

    Add LeetCode submission: Most Frequent Even Element

---
+++ leetcode_submissions/2022-09-11/Most_Frequent_Even_Element_cpp.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int size=1e5+1;
+    int mostFrequentEven(vector<int>& nums) {
+        
+        map<int,int> mp;int m=0,ans;
+        
+        for(int i:nums) {
+            if(i%2==0) mp[i]++;
+        }
+        
+        if(mp.empty()) return -1;
+        
+        for(auto it=mp.rbegin();it!=mp.rend();it++) {
+            if(it->second>=m) {
+                m=it->second;
+                ans=it->first;
+            }
+        }
+        
+        return ans;
+    }
+};
