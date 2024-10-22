b5a9e30a36896f4371c0dd75d916eaeb1ff7c2af
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:59:23 +0530

    Add LeetCode submission: Decode the Message

---
+++ leetcode_submissions/2022-07-03/Decode_the_Message_cpp.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        
+        map<char,char> mp;int x=97;string ans="";
+        
+        for(char c:key) {
+            
+            if(c == ' ') continue;
+            
+            if(!mp.count(c))  mp[c]=(char)x++;
+        }
+        
+        for(char c : message) {
+            
+            if(c==' ') ans+=' ';
+            
+            else ans+=mp[c];
+        }
+        return ans;
+    }
+};
