34e4d8ddd797ca63e649cbe2333778371f61622a
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:52:54 +0530

    Update LeetCode submission: Find First and Last Position of Element in Sorted Array

--- leetcode_submissions/2024-07-22/Find_First_and_Last_Position_of_Element_in_Sorted_Array_cpp.cpp
+++ leetcode_submissions/2024-07-22/Find_First_and_Last_Position_of_Element_in_Sorted_Array_cpp.cpp
@@ -1,56 +1,16 @@
 class Solution {
 public:
-    int findLower(vector<int>& nums, int target) {
-        int lo = 0;
-        int hi = nums.size() -1;
-        int ans = nums.size();
-        
-
-        while(lo<=hi) {
-            int mid = (hi + lo)/2;
-
-            if(nums[mid]>=target) {
-                ans=mid;
-                hi=mid-1;
-            }
-            else {
-                lo=mid+1;
-            }
-        }
-        return ans;
-    }
-
-    int findUpper(vector<int>& nums, int target) {
-        int lo = 0;
-        int hi = nums.size() -1;
-        int ans = nums.size();
-        
-
-        while(lo<=hi) {
-            int mid = (hi + lo)/2;
-
-            if(nums[mid]>target) {
-                ans=mid;
-                hi=mid-1;
-            }
-            else {
-                lo=mid+1;
-            }
-        }
-        return ans;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans = {-1,-1};
         if (nums.empty() || target>nums[nums.size()-1]) return ans;
 
-        int lower = findLower(nums, target);
+        int lower = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
         
         if (nums[lower] != target) {
             return ans;
         }
 
-        int upper = findUpper(nums, target);
+        int upper = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
         ans[0] =  lower;
         ans[1] = upper-1;
 
