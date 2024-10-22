ee9b3dd14c1b2eb4f6963eee3a559189febfe5a4
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:57:34 +0530

    Update LeetCode submission: Maximum Subarray

--- leetcode_submissions/2022-08-20/Maximum_Subarray_cpp.cpp
+++ leetcode_submissions/2022-08-20/Maximum_Subarray_cpp.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
-        int sum=0,ans=INT_MIN;
-        
-        if(nums.size() == 1)
+        if(nums.size()==1){
             return nums[0];
-        
-        for(int i=0;i<nums.size();i++) {
-            
-            sum = max(0 , nums[i]+sum);
-            
-            ans= max(ans,sum);
-            
         }
-        return ans;
+        int max_sub_sum = *max_element(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();++i){
+            nums[i]+=nums[i-1];
+        }
+        nums.insert(nums.begin(),0);
+        int l = 0,r = 0;
+        while(r<nums.size()-1){
+            r++;
+            while(l<r && nums[r]<nums[l]){
+                l++;
+            }
+            if(l!=r) {
+                max_sub_sum = max(max_sub_sum,nums[r]-nums[l]);
+            }
+        }
+        return max_sub_sum;
     }
 };
