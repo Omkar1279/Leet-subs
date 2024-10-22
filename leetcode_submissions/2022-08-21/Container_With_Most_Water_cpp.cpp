76b1232a71374aebd09f150bc7779b397ea31dcc
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:57:14 +0530

    Update LeetCode submission: Container With Most Water

--- leetcode_submissions/2022-08-21/Container_With_Most_Water_cpp.cpp
+++ leetcode_submissions/2022-08-21/Container_With_Most_Water_cpp.cpp
@@ -5,7 +5,8 @@ public:
         int l=0,n=height.size()-1 ,ans=INT_MIN;
         
         while(l<n) {
-            ans=max(ans,(n-l)*min(height[l],height[n]));
+            int area=(n-l)*min(height[l],height[n]);
+            ans=max(ans,area);
             
             if(height[l]>height[n]) n--;
             else l++;
