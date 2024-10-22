367878ea6a1a3827355b42727a2ff5400966e3b5
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-22 18:59:38 +0530

    Update LeetCode submission: Find the Difference of Two Arrays

--- leetcode_submissions/2022-06-29/Find_the_Difference_of_Two_Arrays_cpp.cpp
+++ leetcode_submissions/2022-06-29/Find_the_Difference_of_Two_Arrays_cpp.cpp
@@ -4,29 +4,26 @@ public:
         
         set<int> s1(nums1.begin(),nums1.end());
         set<int> s2(nums2.begin(),nums2.end());
-        set<int> s;
         
         vector<vector<int>>ans;
-
+        
+        vector<int> temp;
         for(int i=0;i<nums1.size();i++) {
             
             if(!s2.count(nums1[i])) {
-                s.insert(nums1[i]);
+                temp.push_back(nums1[i]);
             }
         }
-        vector<int> temp(s.begin(),s.end());
         ans.push_back(temp);
-        s.clear();
+        temp.clear();
         
         for(int i=0;i<nums2.size();i++) {
             
             if(!s1.count(nums2[i])) {
-                s.insert(nums2[i]);
+                temp.push_back(nums2[i]);
             }
         }
-        
-        vector<int> temp1(s.begin(),s.end());
-        ans.push_back(temp1);
+        ans.push_back(temp);
         
         return ans;
     }
