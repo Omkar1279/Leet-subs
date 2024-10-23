c7f2eb1f89fd323078049bf92cc1e6965987d2a4
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:43:58 +0530

    Add LeetCode submission: Array With Elements Not Equal to Average of Neighbors

---
+++ leetcode_submissions/2021-08-15/2085_Array_With_Elements_Not_Equal_to_Average_of_Neighbors/Array_With_Elements_Not_Equal_to_Average_of_Neighbors.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    vector<int> index;
+    
+    bool avg(vector<int> &v) {
+        
+        for(int i=0;i<v.size();i++) {
+            
+            
+            if(i>0 && i<v.size()-1) {
+                
+                if(v[i] == (float)((v[i+1]+v[i-1])/2))
+                    index.push_back(i);
+            }
+            
+            else
+                continue;
+        }
+        
+        return (index.size() == 0) ? 0 : 1;
+    }
+    
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        if(nums.size()==3 && avg(nums)) {
+            swap(nums[0],nums[1]);
+            
+        }
+        
+        while(nums.size()==4 && avg(nums)) {
+            swap(nums[0],nums[1]);
+            swap(nums[1],nums[2]);
+            
+        }
+        
+        if(nums.size()==3 || nums.size()==4)
+        return nums;
+        
+        while(avg(nums)) {
+            
+            for(int i=0;i<index.size();i++) {
+                
+                if(i+1<index.size())
+                swap(nums[i],nums[i+1]);
+            }
+            index.clear();
+        }
+        return nums;
+    }
+};
