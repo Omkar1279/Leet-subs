22e313ed680f8dc36d7a379be878c63aa4d74803
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:35:08 +0530

    Update LeetCode submission: Merge Intervals

--- leetcode_submissions/2022-05-29/56_Merge_Intervals/Merge_Intervals.cpp
+++ leetcode_submissions/2022-05-29/56_Merge_Intervals/Merge_Intervals.cpp
@@ -5,7 +5,7 @@ public:
         int n=in.size();
         
         if(n<=1) return in;
-        vector<vector<int>> ans;
+        vector<vector<int>> ans;vector<int> v;
         
         sort(in.begin(),in.end());
 
@@ -13,7 +13,10 @@ public:
             
             if(ans.empty()) {
                 if((in[i-1][0] <= in[i][0]) && (in[i][0] <= in[i-1][1])) {
-                    ans.push_back({min(in[i-1][0],in[i][0]),max(in[i-1][1],in[i][1])});
+                    v.push_back(min(in[i-1][0],in[i][0]));
+                    v.push_back(max(in[i-1][1],in[i][1]));
+                    ans.push_back(v);
+                    v.clear();
                 }
                 else {
                     ans.push_back(in[i-1]);
@@ -23,9 +26,12 @@ public:
             
             int start = ans.back()[0],end = ans.back()[1];
             if((start <= in[i][0]) && (in[i][0] <= end)) {
+                
+                v.push_back(min(start,in[i][0]));
+                v.push_back(max(end,in[i][1]));
                 ans.pop_back();
-                ans.push_back({min(start,in[i][0]),max(end,in[i][1])});
-
+                ans.push_back(v);
+                v.clear();
             }
             else {
                 ans.push_back(in[i]);
