cdeab1aabaff8ec9a81abad55a7f3f12cf87ad12
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:28:33 +0530

    Update LeetCode submission: Design a Food Rating System

--- leetcode_submissions/2022-07-24/2429_Design_a_Food_Rating_System/Design_a_Food_Rating_System.cpp
+++ leetcode_submissions/2022-07-24/2429_Design_a_Food_Rating_System/Design_a_Food_Rating_System.cpp
@@ -1,46 +1,33 @@
 class FoodRatings {
 public:
-    map<string,vector<pair<string,int>>> mp;
-    
-    vector<string> v;vector<string> c;
+    map<string,pair<string,int>> mp;
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         
         for(int i=0;i<foods.size();i++) {
-            mp[cuisines[i]].push_back({foods[i],ratings[i]});
-            sort(mp[cuisines[i]].begin(),mp[cuisines[i]].end());
+            mp[foods[i]] = {cuisines[i],ratings[i]};
         }
-        v=foods;c=cuisines;
     }
     
     void changeRating(string food, int newRating) {
         
-        int i=0;
-        for(;i<v.size();i++) {
-            if(v[i]==food) break;
-        }
-        vector<pair<string,int>> temp;
-        temp=mp[c[i]];
-        
-        for(int j=0;j<temp.size();j++) {
-            if(temp[j].first==food) temp[j].second=newRating;
-            break;
-        }
-        mp[c[i]]=temp;
-        
+        pair<string,int> p;
+        p=mp[food];
+        p.second=newRating;
+        mp[food]=p;
     }
     
     string highestRated(string cuisine) {
         
-        int maxi=INT_MIN;vector<pair<string,int>> ans;string sol;
-        ans=mp[cuisine];
-        
-        for(int i=0;i<ans.size();i++) {
+        int maxi=INT_MIN;string ans;
+        for(auto it=mp.begin();it!=mp.end();it++) {
             
-            if(ans[i].second > maxi) {
-                sol=ans[i].first;maxi=ans[i].second;
+            if(it->second.first==cuisine) {
+                if(it->second.second > maxi) {
+                    ans=it->first;maxi=it->second.second;
+                }
             }
         }
-        return sol;
+        return ans;
     }
 };
 
