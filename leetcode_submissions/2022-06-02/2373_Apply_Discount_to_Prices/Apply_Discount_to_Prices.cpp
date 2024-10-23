f58581811baf8568efffcb3304337fd36e98c897
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:34:13 +0530

    Add LeetCode submission: Apply Discount to Prices

---
+++ leetcode_submissions/2022-06-02/2373_Apply_Discount_to_Prices/Apply_Discount_to_Prices.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string help(string temp,int d) {
+        int num=stoi(temp);
+        
+        double x=(double)(100 - d) / 100.0;
+        x=x*num;
+        
+        stringstream ss2;
+        ss2 << fixed << setprecision(2) << (x);
+        
+        return ss2.str();
+    }
+    
+    string discountPrices(string sen, int discount) {
+        
+        int n=sen.size();string ans;string temp;
+        
+        int i=0;
+        for(i=0;i<n;i++) {
+            int flag=1;
+            if(sen[i] == '$' && (i==0 || sen[i-1] == ' ')) {
+
+                if(i+1<n) {
+                    int j=i+1;
+
+                    while(sen[j]!=' ' && j<n) {
+
+                        if(!isdigit(sen[j])) {flag=0;break;}
+                        temp+=sen[j];j++;
+                    }
+                    if(flag) {
+                        temp=help(temp,discount);
+                        ans+='$';
+                        ans+=temp;
+                        i=j;
+                    }
+                }
+            }
+            ans+=sen[i];
+        }
+        return ans;
+    }
+};
