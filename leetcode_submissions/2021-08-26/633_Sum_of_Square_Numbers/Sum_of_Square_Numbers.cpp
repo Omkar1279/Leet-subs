6143e122c62c6a51e8369eb07a59a04865ad14fc
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:43:27 +0530

    Add LeetCode submission: Sum of Square Numbers

---
+++ leetcode_submissions/2021-08-26/633_Sum_of_Square_Numbers/Sum_of_Square_Numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        
+        if(c<2) return 1;
+               
+        for(int i=0;i<c;i++) {
+            
+            if((i*i) > c) return 0;
+            
+            if(sqrt(c))
+            
+            for(int j=i;j<c;j++) {
+                
+                if((j*j) == c) return 1;
+
+                if((j*j) > c ) break;
+                
+                if(c==(j*j) + (i*i)) return 1;
+                
+            }
+        }
+        return 0;
+    }
+};
