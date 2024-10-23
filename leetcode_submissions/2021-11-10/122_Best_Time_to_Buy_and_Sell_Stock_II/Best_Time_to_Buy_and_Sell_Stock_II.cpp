ce458e955845dca9fa0fd1d3a78dd9cb4c00da9e
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:39:01 +0530

    Add LeetCode submission: Best Time to Buy and Sell Stock II

---
+++ leetcode_submissions/2021-11-10/122_Best_Time_to_Buy_and_Sell_Stock_II/Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int sum=0;
+
+        
+        for(int i=1;i<prices.size();i++) {
+            
+            if(prices[i]-prices[i-1] > 0) sum+=(prices[i]-prices[i-1]);
+            
+        }
+        
+        if(sum<0) return 0;
+        
+        return sum;
+        
+    }
+};
