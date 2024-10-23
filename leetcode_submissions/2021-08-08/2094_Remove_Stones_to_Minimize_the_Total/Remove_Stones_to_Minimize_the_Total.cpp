ab780ec6f0f75f8255f56d7468a78ece58527af0
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:45:01 +0530

    Add LeetCode submission: Remove Stones to Minimize the Total

---
+++ leetcode_submissions/2021-08-08/2094_Remove_Stones_to_Minimize_the_Total/Remove_Stones_to_Minimize_the_Total.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int floor(int x) {
+        return (x+1)/2;
+    }
+    
+    int minStoneSum(vector<int>& piles, int k) {
+ 
+        sort(piles.begin(), piles.end(), greater<int>());
+        
+        for(int i=0;i<k;i++) {
+            
+            if(piles[i] == floor(piles[i]))
+                i++;
+            
+            piles[i]=(int)floor(piles[i]);
+            
+        }
+            
+        
+        
+        return accumulate(piles.begin() , piles.end() , 0);
+    }
+};
