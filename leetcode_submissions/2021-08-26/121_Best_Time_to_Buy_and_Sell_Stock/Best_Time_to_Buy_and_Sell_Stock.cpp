bb69c7d54726de31a47e06b52e48c0a6572e3baf
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:43:09 +0530

    Add LeetCode submission: Best Time to Buy and Sell Stock

---
+++ leetcode_submissions/2021-08-26/121_Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size(),m=INT_MAX,profit=INT_MIN;
+
+        
+        for(int i=0;i<n;i++) {
+     
+            m=min(m,prices[i]);
+            profit=max(profit,(prices[i]-m));
+            
+        }
+        
+        if(profit<0) return 0;
+        
+        return profit;
+    }
+};
