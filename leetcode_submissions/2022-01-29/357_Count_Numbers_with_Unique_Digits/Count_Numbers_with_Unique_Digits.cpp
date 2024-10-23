85cc34cb62de5c7c2f5633b6ed53976d1bb5fc17
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:36:35 +0530

    Add LeetCode submission: Count Numbers with Unique Digits

---
+++ leetcode_submissions/2022-01-29/357_Count_Numbers_with_Unique_Digits/Count_Numbers_with_Unique_Digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+int countNumbersWithUniqueDigits(int n) {
+    int dp=1;
+    int tmp=9;
+    for(int i=1;i<=n&&i<10;i++){
+        dp+=tmp;
+        tmp*=(10-i);
+    }
+    return dp;
+}
+};
