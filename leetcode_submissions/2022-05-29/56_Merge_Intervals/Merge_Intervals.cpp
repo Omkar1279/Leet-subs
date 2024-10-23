6c7c467c5840476d270550dfc095ac8b83b94e2e
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:35:05 +0530

    Add LeetCode submission: Merge Intervals

---
+++ leetcode_submissions/2022-05-29/56_Merge_Intervals/Merge_Intervals.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        
+        int n=in.size();
+        
+        if(n<=1) return in;
+        vector<vector<int>> ans;
+        
+        sort(in.begin(),in.end());
+
+        for(int i=1;i<n;i++) {
+            
+            if(ans.empty()) {
+                if((in[i-1][0] <= in[i][0]) && (in[i][0] <= in[i-1][1])) {
+                    ans.push_back({min(in[i-1][0],in[i][0]),max(in[i-1][1],in[i][1])});
+                }
+                else {
+                    ans.push_back(in[i-1]);
+                    ans.push_back(in[i]);
+                }
+            }
+            
+            int start = ans.back()[0],end = ans.back()[1];
+            if((start <= in[i][0]) && (in[i][0] <= end)) {
+                ans.pop_back();
+                ans.push_back({min(start,in[i][0]),max(end,in[i][1])});
+
+            }
+            else {
+                ans.push_back(in[i]);
+            }
+
+        }
+        return ans;
+
+    }
+};
