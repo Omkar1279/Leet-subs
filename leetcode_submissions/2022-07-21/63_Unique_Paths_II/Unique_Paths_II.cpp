880cdc4c995556f7a61c351a02ea814ee1c174d2
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:29:07 +0530

    Update LeetCode submission: Unique Paths II

--- leetcode_submissions/2022-07-21/63_Unique_Paths_II/Unique_Paths_II.cpp
+++ leetcode_submissions/2022-07-21/63_Unique_Paths_II/Unique_Paths_II.cpp
@@ -6,14 +6,13 @@ public:
         
         int dp[n][m];
         
-        // if(n==1 && m==1) return 0;
-        // else if(n==1 && m==1 && obstacleGrid[0][0]==0) return 1;
+        if(n==1 && m==1) return 0;
         
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 
-                if (obstacleGrid[i][j] == 1) dp[i][j]=0;
-                else if (i==0 && j==0) dp[i][j]=1;
+                if(i==0 && j==0) dp[i][j]=1;
+                else if(obstacleGrid[i][j] == 1) dp[i][j]=0;
                 
                 else {
                     dp[i][j]= ((i>0) ? dp[i-1][j] : 0) + ((j>0) ? dp[i][j-1] : 0);
