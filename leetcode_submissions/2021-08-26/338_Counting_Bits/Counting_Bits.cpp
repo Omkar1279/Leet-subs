edd2592b704448b6fd40515652e9061b14c373fb
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:42:44 +0530

    Update LeetCode submission: Counting Bits

--- leetcode_submissions/2021-08-26/338_Counting_Bits/Counting_Bits.cpp
+++ leetcode_submissions/2021-08-26/338_Counting_Bits/Counting_Bits.cpp
@@ -4,9 +4,13 @@ public:
         
         vector<int>v(n+1);
         
-        v[0]=0;  if(n==0) return v;
+        v[0]=0;
         
-        v[1]=1;  if(n==1) return v;
+        if(n==0) return v;
+        
+        v[1]=1;
+        
+        if(n==1) return v;
         
         for(int i=2;i<=n;i++) {
             
