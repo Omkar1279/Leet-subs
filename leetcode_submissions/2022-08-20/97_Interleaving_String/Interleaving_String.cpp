fbd29a126bb4d6a7107ebe44b0a0c89f8940b4b4
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:25:40 +0530

    Add LeetCode submission: Interleaving String

---
+++ leetcode_submissions/2022-08-20/97_Interleaving_String/Interleaving_String.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool f(int i,int j,string s1, string s2, string s3,vector<vector<int>> &dp) {
+        
+        if(i==s1.size() && j==s2.size()) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];        
+        
+        bool res=0;
+        if(i<s1.size() and s1[i]==s3[i+j] && j<s2.size() and s2[j]==s3[i+j]) 
+            res= f(i+1,j,s1,s2,s3,dp) || f(i,j+1,s1,s2,s3,dp);
+        else if(i<s1.size() and s1[i]==s3[i+j]) res=f(i+1,j,s1,s2,s3,dp);
+        else if(j<s2.size() and s2[j]==s3[i+j]) res=f(i,j+1,s1,s2,s3,dp);
+        
+        return dp[i][j]= res;
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        
+        if(s1.size()+s2.size()!=s3.size()) return 0;
+        
+        return f(0,0,s1,s2,s3,dp);
+    }
+};
