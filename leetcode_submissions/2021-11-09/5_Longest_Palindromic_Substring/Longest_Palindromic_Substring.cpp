e06c46034cc31fb72c078a5028b61b1333074d72
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:39:15 +0530

    Update LeetCode submission: Longest Palindromic Substring

--- leetcode_submissions/2021-11-09/5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.cpp
+++ leetcode_submissions/2021-11-09/5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.cpp
@@ -1,29 +1,25 @@
-class Solution{
+class Solution {
 public:
-    string longestPalindrome(string& s)
-    {
-        int n = s.length();
-        int maxlen = 1, left = 0;
+    string longestPalindrome(string s) {
         
-        if(n == 1)
-            return string(1, s[0]);
+        if(s.length() <= 1) return s;
         
-        int DP[n][n];
+        int n=s.length(),maxlen,left;
         
-        for(int i = 0; i < n; i++)
-        {
-            DP[i][i] = true;
+        int dp[1005][1005];
+        
+
+        for(int i = 0; i < n; i++) {
             
-            for(int j = 0; j < i; j++)
-            {
-                if(s[i] == s[j] && (i - j < 2 || DP[j + 1 ][i - 1]) )// ( i - j < 2) means if two values are adjacent
-                                                                    //DP[j + 1 ][i - 1] means check values from i to j whether is a palindromic or not. 
-                    DP[j][i] = true;
-                else
-                    DP[j][i] = false;
+            dp[i][i] = true;
+            
+            for(int j = 0; j < i; j++) {
                 
-                if(DP[j][i] && i - j + 1 > maxlen)
-                {
+                if(s[i] == s[j] && (i - j < 2 || dp[j + 1 ][i - 1]) ) dp[j][i] = true;
+                
+                else  dp[j][i] = false;
+                
+                if(dp[j][i] && i - j + 1 > maxlen) {
                     maxlen = i - j + 1;
                     left = j;
                 }
