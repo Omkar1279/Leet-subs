40bf05039c6acaeceedcc0f23937c422717f754d
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:23:21 +0530

    Add LeetCode submission: The Employee That Worked on the Longest Task

---
+++ leetcode_submissions/2022-10-11/2518_The_Employee_That_Worked_on_the_Longest_Task/The_Employee_That_Worked_on_the_Longest_Task.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        
+        int size=logs.size(),max=0,id=0;
+        
+        for(int i=0;i<size;i++) {
+            
+            if(i==0) {max=logs[i][1];id=logs[i][0];}
+            
+            else {
+                if((logs[i][1]-logs[i-1][1]) > max) {max = logs[i][1]-logs[i-1][1];id=logs[i][0];}
+                else if((logs[i][1]-logs[i-1][1]) == max) id= min(id,logs[i][0]);
+            }
+        }
+        return id;
+    }
+};
