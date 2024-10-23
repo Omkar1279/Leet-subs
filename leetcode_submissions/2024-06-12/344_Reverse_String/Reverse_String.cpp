680b1d3b3571c0f5790d6de1ab701291d19e463f
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:19:30 +0530

    Update LeetCode submission: Reverse String

--- leetcode_submissions/2024-06-12/344_Reverse_String/Reverse_String.cpp
+++ leetcode_submissions/2024-06-12/344_Reverse_String/Reverse_String.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l=0, n=s.size(), h=n-1;
-
-        while(l<h) {
-            swap(s[l], s[h]);
-            l++;h--;
-        }
+        return reverse(s.begin(), s.end());
     }
 };
