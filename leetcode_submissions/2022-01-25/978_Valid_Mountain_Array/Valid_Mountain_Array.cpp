a7293b560cc42e1b269a3aaf364c4551d3542bec
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:37:11 +0530

    Add LeetCode submission: Valid Mountain Array

---
+++ leetcode_submissions/2022-01-25/978_Valid_Mountain_Array/Valid_Mountain_Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int i=0,j=n-1;
+        //mountain array if and only if: arr.length >= 3
+        //should start with decreasing order
+        if(n<3 || arr[0]>arr[1])return false;
+        
+        //found first decreasing number from begin
+        while(i<n-1&&arr[i]<arr[i+1]){
+            i++;
+        }
+        //found first decreasing number from end
+        while(j>0&&arr[j]<arr[j-1]){
+            j--;
+        }
+        
+        //it should be same
+        return i>0 && i==j && j<n-1;
+    }
+};
