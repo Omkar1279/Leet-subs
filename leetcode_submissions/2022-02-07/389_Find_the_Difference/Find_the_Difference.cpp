7e42df39d84e07710f347db881c443b93d431252
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:36:02 +0530

    Update LeetCode submission: Find the Difference

--- leetcode_submissions/2022-02-07/389_Find_the_Difference/Find_the_Difference.cpp
+++ leetcode_submissions/2022-02-07/389_Find_the_Difference/Find_the_Difference.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
+    
     char findTheDifference(string s, string t) {
         
-        for(int i=0;i<s.size();i++)
-            t[i+1]+=t[i]-s[i];
+        if(s == "") {
+            char c= t[0];
+            return c;
+        }
+        
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+        
+        for(int i=0;i<s.size();i++) {
+            
+            if(s[i] != t[i]) {
+                return t[i];
+            }
+            
+        }
         return t[t.size()-1];
     }
 };
