cc37dd8d1ea4922aff5b48ea976563c5d9641ca6
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:34:09 +0530

    Update LeetCode submission: Range Sum Query 2D - Immutable

--- leetcode_submissions/2022-06-03/304_Range_Sum_Query_2D_-_Immutable/Range_Sum_Query_2D_-_Immutable.cpp
+++ leetcode_submissions/2022-06-03/304_Range_Sum_Query_2D_-_Immutable/Range_Sum_Query_2D_-_Immutable.cpp
@@ -1,25 +1,37 @@
 class NumMatrix {
 public:
-    vector<vector<int>> mat;
+    vector<vector<int>> sum;
+    
     NumMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(),n = matrix[0].size();
-        for(int i = 0 ; i < m; i++){
-            vector<int> row(n);
-            row[0] = matrix[i][0];
-            for(int j = 1 ; j < n; j++){
-                row[j] = row[j-1] + matrix[i][j];
+        int m = matrix.size(), n = matrix[0].size();
+        sum = vector<vector<int>>(m, vector<int>(n));
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                
+                if(j==0) sum[i][j]=matrix[i][j];
+                
+                else {
+                    sum[i][j] =sum[i][j-1] + matrix[i][j];
+                }
             }
-            mat.push_back(row);
         }
+        
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0,minus = 0;
-        for(int i = row1 ; i <= row2; i++){
-            sum += mat[i][col2];
-            if(col1 > 0)
-                minus += mat[i][col1-1];
+        
+        int ans=0;
+        for(int i=row1;i<=row2;i++) {
+            
+            ans+= sum[i][col2] - sum[i][col1-1];
         }
-        return sum - minus;
+        return ans;
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
