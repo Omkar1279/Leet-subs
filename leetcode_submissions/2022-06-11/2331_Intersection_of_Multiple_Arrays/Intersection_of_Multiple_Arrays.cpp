ae97f29cbad760f5ccad681ae9958eb5671d4215
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:32:25 +0530

    Update LeetCode submission: Intersection of Multiple Arrays

--- leetcode_submissions/2022-06-11/2331_Intersection_of_Multiple_Arrays/Intersection_of_Multiple_Arrays.cpp
+++ leetcode_submissions/2022-06-11/2331_Intersection_of_Multiple_Arrays/Intersection_of_Multiple_Arrays.cpp
@@ -12,9 +12,9 @@ public:
             }
         }
         
-        for(int i:nums[0]) {
+        for(int i=0;i<nums[0].size();i++) {
             
-            if(mp[i] == nums.size()) v.push_back(i);
+            if(mp[nums[0][i]] == nums.size()) v.push_back(nums[0][i]);
         }
         sort(v.begin(),v.end());
         
