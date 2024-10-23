7f96e9766deade96d61a8b91b0492119df420304
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:17:52 +0530

    Update LeetCode submission: Generate Binary Strings Without Adjacent Zeros

--- leetcode_submissions/2024-07-17/3453_Generate_Binary_Strings_Without_Adjacent_Zeros/Generate_Binary_Strings_Without_Adjacent_Zeros.cpp
+++ leetcode_submissions/2024-07-17/3453_Generate_Binary_Strings_Without_Adjacent_Zeros/Generate_Binary_Strings_Without_Adjacent_Zeros.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    vector<string> ans;
-    void f(int prev, string &str, int n){
-        if(str.length() == n){
-            ans.push_back(str);
-            return;
-        }
-        str.push_back('1');
-        f(1, str, n);
-        str.pop_back();
-        if(prev == 1){
-            str.push_back('0');
-            f(0, str, n);
-            str.pop_back();
-        } 
-    }
     vector<string> validStrings(int n) {
-        string a = "0", b = "1";
-        ans.clear();
-        f(0, a, n);
-        f(1, b, n);
-        return ans;
+
+        if(n==1) return vector<string> {"0", "1"};
+
+        
+        vector<string> prev= {"0", "1"};
+
+        for(int i=1; i< n; i++) {
+            vector<string> cur;
+            for(int j=0;j<prev.size(); j++) {
+                string p = prev[j];
+                if(p[p.size()-1] == '0') {
+                    p+='1';
+                    cur.push_back(p);
+                }
+
+                else {
+                    string temp =p;
+                    p+='0';
+                    temp+='1';
+                    cur.push_back(p);
+                    cur.push_back(temp);
+                }
+            }
+            prev = cur;
+        }
+        return prev;
     }
 };
