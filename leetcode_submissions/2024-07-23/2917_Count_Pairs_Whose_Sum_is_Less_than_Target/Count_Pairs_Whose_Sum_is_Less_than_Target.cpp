3481380fd10a50e4f738b52809509205b3dfa078
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:09:00 +0530

    Update LeetCode submission: Count Pairs Whose Sum is Less than Target

--- leetcode_submissions/2024-07-23/2917_Count_Pairs_Whose_Sum_is_Less_than_Target/Count_Pairs_Whose_Sum_is_Less_than_Target.cpp
+++ leetcode_submissions/2024-07-23/2917_Count_Pairs_Whose_Sum_is_Less_than_Target/Count_Pairs_Whose_Sum_is_Less_than_Target.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        // Sort the array to use the two-pointer technique
+        int n= nums.size(), ans=0;
         sort(nums.begin(), nums.end());
-        
-        // Call the recursive helper function with initial pointers
-        return countPairsHelper(nums, target, 0, nums.size() - 1);
-    }
 
-private:
-    int countPairsHelper(const vector<int>& nums, int target, int lo, int hi) {
-        // Base case: if the pointers cross, no more pairs to count
-        if (lo >= hi) {
-            return 0;
+        int lo=0,hi=n-1;
+
+        while(lo<hi) {
+            if(nums[lo] + nums[hi] < target) {
+                ans+= (hi-lo);
+                lo++;
+            }
+            else hi--;
         }
-        
-        // If the sum of the two elements is less than the target
-        if (nums[lo] + nums[hi] < target) {
-            // Count all pairs (nums[lo], nums[j]) for lo < j <= hi
-            // and move the lower pointer up
-            return (hi - lo) + countPairsHelper(nums, target, lo + 1, hi);
-        } else {
-            // If the sum is not less than the target, move the higher pointer down
-            return countPairsHelper(nums, target, lo, hi - 1);
-        }
+        return ans;
     }
 };
