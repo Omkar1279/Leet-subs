3f80ccf10106a4f80df898d070e369df7a905e05
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:41:02 +0530

    Update LeetCode submission: Same Tree

--- leetcode_submissions/2021-09-02/100_Same_Tree/Same_Tree.cpp
+++ leetcode_submissions/2021-09-02/100_Same_Tree/Same_Tree.cpp
@@ -11,45 +11,16 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* p,vector<int> &t) {
-        
-        stack<pair<TreeNode*,int>>s;
-        
-        s.push({p,0});
-        
-        while(!s.empty()) {
-            
-            TreeNode* cur = s.top().first;
-            int c = s.top().second;
-            s.pop();
-            
-            if(cur == nullptr) t.push_back(-1);
-            
-            if(cur == NULL || c >= 3) continue;
-            
-            s.push({cur,c+1});
-            
-            if(c == 0) t.push_back(cur->val);
-            else if( c== 1) s.push({cur->left,0});
-            else if( c== 2) s.push({cur->right,0});
-            
-        }
-    }
-    
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        if(p && !q) return 0;
-        else if(!p && q) return 0;
+        if(p && !q || !p && q) return 0;
         else if(!p && !q) return 1;
         
-        vector<int> s;
-        vector<int> t;
-        dfs(p,s);
-        dfs(q,t);
+        if(p->val == q->val) {
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        }
         
-        if(s != t) return 0;
-        
-        else return 1;
+        else return 0;
         
     }
 };
