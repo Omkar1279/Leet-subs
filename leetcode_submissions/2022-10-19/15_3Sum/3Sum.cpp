3ffafe3f75324908f8172cc9bf78f4ee868c0b51
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:22:22 +0530

    Update LeetCode submission: 3Sum

--- leetcode_submissions/2022-10-19/15_3Sum/3Sum.cpp
+++ leetcode_submissions/2022-10-19/15_3Sum/3Sum.cpp
@@ -20,7 +20,7 @@ public:
                     ans.push_back(vector<int>{nums[i],nums[l],nums[r]});
                     
                     while(l<r && nums[l]==nums[l+1]) l++;
-                    while(l<r && nums[r]==nums[r-1]) r--;
+                    while(l<r && nums[r]==nums[r+1]) r--;
                     
                     l++;r--;
                 }
