f566e215daf6001169e5733289a12ae8ffcdfb1e
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:33:06 +0530

    Add LeetCode submission: Percentage of Letter in String

---
+++ leetcode_submissions/2022-06-09/2365_Percentage_of_Letter_in_String/Percentage_of_Letter_in_String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        
+        int cnt=0;
+        unordered_map<char,int>m;
+        
+        for(auto ch : s) 
+            m[ch]++;
+        
+        cnt = m[letter];
+
+        double percent = (double)cnt/s.size();
+        int ans = percent*100;
+        return ans;
+    }
+};
