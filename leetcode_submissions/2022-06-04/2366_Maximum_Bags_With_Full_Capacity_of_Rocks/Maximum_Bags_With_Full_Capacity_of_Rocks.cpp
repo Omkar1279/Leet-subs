0dbecb17adfc1f9f59598bd82213654f8435b2fd
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:33:51 +0530

    Add LeetCode submission: Maximum Bags With Full Capacity of Rocks

---
+++ leetcode_submissions/2022-06-04/2366_Maximum_Bags_With_Full_Capacity_of_Rocks/Maximum_Bags_With_Full_Capacity_of_Rocks.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        
+        int i=0,ans=0;
+        vector<int>v(rocks.size());
+        
+        for(int i=0;i<rocks.size();i++)  v[i]=(capacity[i]-rocks[i]);
+        
+        // for(int i=0;i<rocks.size();i++) cout<<v[i]<<endl;
+        
+        sort(v.begin(),v.end());
+        
+        while(i< rocks.size()) {
+                       
+            if(v[i] == 0) ans++;
+            
+            else if ((additionalRocks-v[i]) >= 0) {
+                additionalRocks-=v[i];
+                // cout<<"2\n";
+                ans++;
+            }
+            // cout<<"i="<<i<<" ans="<<ans<<endl;
+            i++;
+        }
+        return ans;
+    }
+};
