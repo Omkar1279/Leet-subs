7bd747e52f290861342d64af2acba38afa892f95
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:44:19 +0530

    Update LeetCode submission: Number of Strings That Appear as Substrings in Word

--- leetcode_submissions/2021-08-15/2099_Number_of_Strings_That_Appear_as_Substrings_in_Word/Number_of_Strings_That_Appear_as_Substrings_in_Word.cpp
+++ leetcode_submissions/2021-08-15/2099_Number_of_Strings_That_Appear_as_Substrings_in_Word/Number_of_Strings_That_Appear_as_Substrings_in_Word.cpp
@@ -4,10 +4,31 @@ public:
         
         int cnt=0,k=0;
         
-        for(auto &c : patterns) {
+        string s="";
+        
+        for(int i=0;i<patterns.size();i++) {
             
-            if(word.find(c) != -1)
-                cnt++;
+            s=patterns[i];
+            
+            for(int j=0;j<word.size();j++) {
+                
+                if(s[k] != word[j]) {
+                    k=0;
+                    continue;
+                }
+                
+                else if(s[k] == word[j]) {
+                    k++;
+                    
+                    if(k==s.size()) {
+                        k=0;
+                        cnt++;
+                        break;
+                    }
+                    continue;
+                }
+                
+            }
         }
         
         return cnt;
