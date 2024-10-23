2d8169975bd8b61c86e3cbc82440b5702892e22d
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:31:44 +0530

    Update LeetCode submission: Minimum Consecutive Cards to Pick Up

--- leetcode_submissions/2022-06-18/2338_Minimum_Consecutive_Cards_to_Pick_Up/Minimum_Consecutive_Cards_to_Pick_Up.cpp
+++ leetcode_submissions/2022-06-18/2338_Minimum_Consecutive_Cards_to_Pick_Up/Minimum_Consecutive_Cards_to_Pick_Up.cpp
@@ -3,7 +3,7 @@ public:
     int minimumCardPickup(vector<int>& cards) {
         
         int n=cards.size(),ans{INT_MAX};
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         
         for(int i=0;i<n;i++) {
             
@@ -11,7 +11,7 @@ public:
             
             else {ans=min(ans,(i-mp[cards[i]])+1);mp[cards[i]]=i;}
         }
-
+        
         return ans==INT_MAX ? -1 : ans;
     }
 };
