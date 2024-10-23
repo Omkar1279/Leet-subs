6af3ae95bf957a1b47230e637cabeedce9b13641
Omkar <69752718+Omkar1279@users.noreply.github.com>
2024-10-23 11:28:11 +0530

    Update LeetCode submission: Climbing Stairs

--- leetcode_submissions/2022-08-07/70_Climbing_Stairs/Climbing_Stairs.cpp
+++ leetcode_submissions/2022-08-07/70_Climbing_Stairs/Climbing_Stairs.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    int f(int n,int dp[]) {
+        
+        if(n<0) return 0;
+        if(n==0) return 1;
+        
+        if(dp[n] != -1) return dp[n];
+        
+        return dp[n]= f(n-1,dp)+f(n-2,dp);
+    }
     
     int climbStairs(int n) {
         
         int dp[n+1];
         memset(dp, -1 ,sizeof(dp));
+        return f(n,dp);
         
-        for(int i=0;i<4;i++) dp[i]=i;
-        
-        for(int i=4;i<=n;i++) {
-            
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
     }
 };
